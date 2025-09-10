fd41fb332fc90d7ac7a8b1cc0bcb62cf90775a35
PNG-32 <142170993+PNG-32@users.noreply.github.com>
2025-09-10 18:11:44 -0300

    More additions

--- ino/data-logger/bits/datalogger.hpp
+++ ino/data-logger/bits/datalogger.hpp
@@ -10,18 +10,36 @@
 
 namespace Bits {
 	struct DataLogger {
+		struct PACKED Info {
+			uint8 firstInit: 1;
+		};
+
 		struct PACKED Log {
 			uint32			timestamp;
 			Sensor::Value	value;
 			uint8_t			luminosity;
 		};
 
-		DataLogger(uint8 const sensorPin = 2, uint8 const ldrPin = A0):
-			clock(0),
+		struct LEDPins {
+			avr_pin
+				red,
+				yellow,
+				green
+			;
+		};
+
+		DataLogger(
+			avr_pin const	sensorPin	= 2,
+			avr_pin const	ldrPin		= A0,
+			LEDPins const&	ledPins		= {3, 4, 5}
+		):
+			info(0),
+			clock(info.end()),
 			sensor(sensorPin, clock.end()),
 			ldr(ldrPin, sensor.end()),
 			db(ldr.end()),
-			display() {}
+			display(),
+			led(ledPins) {}
 
 		void begin() {
 			Serial.begin(9600);
@@ -30,6 +48,9 @@ namespace Bits {
 			display.begin();
 			if (!clock.adjusted())
 				clock.adjust({F(__DATE__), F(__TIME__)});
+			pinMode(led.red,	OUTPUT);
+			pinMode(led.yellow,	OUTPUT);
+			pinMode(led.green,	OUTPUT);
 		}
 
 		void update() {
@@ -41,12 +62,15 @@ namespace Bits {
 		}
 
 	private:
+		Record<Info>	info;
 		Clock			clock;
 		Sensor			sensor;
 		LDR				ldr;
 		DataBank<Log>	db;
 		Display			display;
 		uint16			cooldown = 0;
+		LEDPins			led;
+
 	};
 }
 
--- ino/data-logger/data-logger.ino
+++ ino/data-logger/data-logger.ino
@@ -3,12 +3,22 @@
 Bits::Display display;
 
 void setup() {
-  display.begin();
+	display.begin();
 	display.setDisplay(Bits::Display::State::BDS_ON);
-	display.setCursor(Bits::Display::Cursor::BDC_BLINKING);
+	//display.setCursor(Bits::Display::Cursor::BDC_BLINKING);
+	display.setCursorPosition(0, 0);
+	display.write("[=++==+==+==++=]");
+	display.setCursorPosition(0, 1);
+	display.write("[=++==+==+==++=]");
+	for (usize i = 0; i < 16; ++i)
+		display.scroll(Bits::Display::TextDirection::BDTD_RIGHT);
 }
 void loop() {
-	display.setCursorPosition(0, 0);
-	display.write("What are you looking at?");
-	Bits::Wait::seconds(1);
+	for (usize i = 0; i < 32; ++i) {
+		Bits::Wait::seconds(0.5);
+		display.scroll(Bits::Display::TextDirection::BDTD_LEFT);
+	}
+  Bits::Wait::seconds(0.5);
+	for (usize i = 0; i < 32; ++i)
+		display.scroll(Bits::Display::TextDirection::BDTD_RIGHT);
 }
